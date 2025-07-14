fabcd64a495aaba988731db55125b4733c81c15c
Prem Kumar <prem9810683167@gmail.com>
2025-07-14 18:48:18 +0530

    final commit

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,22 @@ import cors from 'cors';
 import formRoutes from './routes/formRoutes.js';
 
 const app = express();
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://form-creator-two.vercel.app'
+];
+
 app.use(cors({
-    origin: "https://form-creator-two.vercel.app/",
-    credentials: true
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
 }));
+
 app.use(express.json());
 app.use('/api/forms', formRoutes);
 
